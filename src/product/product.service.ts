3c79ff61d02fa6ec7c9621cb9fa6fd5ef3735e75
KryptomSilver <32494744+KryptomSilver@users.noreply.github.com>
2021-11-16 12:21:42 -0600

    Create get product in product service

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { productDTO } from './dto/product.dto';
 import { IProduct } from './interfaces/product.interface';
 
 @Injectable()
@@ -11,5 +12,9 @@ export class ProductService {
   async getProducts(): Promise<IProduct[]> {
     const products = await this.productModel.find();
     return products;
+  }
+  async getProduct(id: string): Promise<IProduct> {
+    const product = await this.productModel.findById(id);
+    return product;
   }
 }
