6b28923ef3fb351afc0a0fbfa8c71ca9ddd2644c
KryptomSilver <32494744+KryptomSilver@users.noreply.github.com>
2021-11-16 12:20:13 -0600

    Create end point get products.

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { IProduct } from './interfaces/product.interface';
 import { ProductService } from './product.service';
 
 @Controller('product')
 export class ProductController {
   constructor(private ProductService: ProductService) {}
+  @Get()
+  async getProducts(): Promise<IProduct[]> {
+    const products = await this.ProductService.getProducts();
+    return products;
+  }
 }
