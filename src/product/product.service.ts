8b64d45560d5b73fdbff16226cb2e032b932264f
KryptomSilver <32494744+KryptomSilver@users.noreply.github.com>
2021-11-16 12:17:07 -0600

    Import product model in product service

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { IProduct } from './interfaces/product.interface';
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+  constructor(
+    @InjectModel('Product') private readonly productModel: Model<IProduct>,
+  ) {}
+}
